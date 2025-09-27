32278837a5e6ad514488e7ca85b4a1b3a65d6a0e
Santiago <santiagovargasve@gmail.com>
2025-09-27 16:30:50 -0500

    first commit

---
+++ apps/expenses-api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  console.log('Running on', 'http://localhost:3000');
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
